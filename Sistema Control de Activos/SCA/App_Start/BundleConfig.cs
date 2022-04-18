18f4b76ec1a367a5bfd9d8fc0ed2602fbf29343f
Genius501 <jarguedas@datasoftla.com>
2022-04-17 20:39:19 -0600

    Cambio de plantilla

--- Sistema Control de Activos/SCA/App_Start/BundleConfig.cs
+++ Sistema Control de Activos/SCA/App_Start/BundleConfig.cs
@@ -8,8 +8,16 @@ namespace SCA
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/plantilla").Include(
+                        "~/Content/lib/easing/easing.min.js",
+                        "~/Content/lib/waypoints/waypoints.min.js",
+                        "~/Content/lib/owlcarousel/owl.carousel.min.js",
+                        "~/Content/lib/tempusdominus/js/moment.min.js",
+                        "~/Content/lib/tempusdominus/js/moment-timezone.min.js",
+                        "~/Content/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js",
+                        "~/Content/lib/isotope/isotope.pkgd.min.js",
+                        "~/Content/lib/lightbox/js/lightbox.min.js",
+                        "~/Content/js/main.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,12 +27,16 @@ namespace SCA
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                     "~/Content/img/favicon.ico",
+                     "~/Content/lib/owlcarousel/assets/owl.carousel.min.css",
+                     "~/Content/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css",
+                     "~/Content/lib/lightbox/css/lightbox.min.css",
+                     "~/Content/css/bootstrap.min.css",
+                     "~/Content/css/style.css"));
         }
     }
 }
