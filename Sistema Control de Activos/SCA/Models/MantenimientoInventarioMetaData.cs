7fdc9ad9a794d644e42297473e9a925ec2811bc6
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2022-05-10 14:44:02 -0600

    As

--- Sistema Control de Activos/SCA/Models/MantenimientoInventarioMetaData.cs
+++ Sistema Control de Activos/SCA/Models/MantenimientoInventarioMetaData.cs
@@ -12,27 +12,27 @@ namespace SCA.Models
     {
         public class MantenimientoInventarioMetaData
         {
-            [Required(ErrorMessage = "Se Debe Ingresar un Codigo")]
-            public int IdInventario { get; set; }
+            //[Required(ErrorMessage = "Se Debe Ingresar un Codigo")]
+            //public int IdInventario { get; set; }
 
-            [Required(ErrorMessage = "Se Debe Seleccionar una Opcion")]
-            public string TipoMantenimiento { get; set; }
+            //[Required(ErrorMessage = "Se Debe Seleccionar una Opcion")]
+            //public string TipoMantenimiento { get; set; }
 
-            [Display(Name = "Costo del Mantenimiento")]
-            public decimal CostoMantenimiento { get; set; }
+            //[Display(Name = "Costo del Mantenimiento")]
+            //public decimal CostoMantenimiento { get; set; }
 
-            [Display(Name = "Fecha de Mantenimiento")]
-            [DataType(DataType.Date)]
-            [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
-            public System.DateTime FechaMantenimiento { get; set; }
+            //[Display(Name = "Fecha de Mantenimiento")]
+            //[DataType(DataType.Date)]
+            //[DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+            //public System.DateTime FechaMantenimiento { get; set; }
 
-            [Display(Name = "Fecha Proximo Mantenimiento")]
-            [DataType(DataType.Date)]
-            [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
-            public System.DateTime ProximoMantenimiento { get; set; }
+            //[Display(Name = "Fecha Proximo Mantenimiento")]
+            //[DataType(DataType.Date)]
+            //[DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+            //public System.DateTime ProximoMantenimiento { get; set; }
 
-            [Display(Name = "Descripcion del Servicio")]
-            public string DescripcionServicio { get; set; }
+            //[Display(Name = "Descripcion del Servicio")]
+            //public string DescripcionServicio { get; set; }
         }
     }
 }
