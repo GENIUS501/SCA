51d59aafcb0fcadfdcd1845b2ac8e8f168bffa7d
Genius501 <jarguedas@datasoftla.com>
2022-05-18 11:57:24 -0600

    as

---
+++ Sistema Control de Activos/SCA/Models/ControlVehiculoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SCA.Models
+{
+    public class ControlVehiculoViewModel
+    {
+        [Display(Name = "Id del control del vehiculo")]
+        public int IdControlVehiculo { get; set; }
+        [Display(Name = "Flotilla")]
+        public Nullable<int> IdFlotilla { get; set; }
+        [Display(Name = "Personal")]
+        public Nullable<int> IdPersonal { get; set; }
+        [Display(Name = "Estado del vehiculo")]
+        [Required(ErrorMessage = "El estado del vehiculo es Requerido")]
+        public int EstadoVehiculo { get; set; }
+        public System.DateTime FechaSalida { get; set; }
+        public int KilometrajeSalida { get; set; }
+        public System.DateTime FechaIngresa { get; set; }
+        public int KilometrajeIngresa { get; set; }
+        public string Anomalias { get; set; }
+    }
+}
