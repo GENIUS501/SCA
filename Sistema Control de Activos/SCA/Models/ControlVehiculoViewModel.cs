e0e34256e16c0ad98363d19d4a8ad2cafa41b028
Genius501 <jarguedas@datasoftla.com>
2022-05-18 12:31:20 -0600

    ASD

--- Sistema Control de Activos/SCA/Models/ControlVehiculoViewModel.cs
+++ Sistema Control de Activos/SCA/Models/ControlVehiculoViewModel.cs
@@ -19,9 +19,22 @@ namespace SCA.Models
         public int EstadoVehiculo { get; set; }
         [Display(Name = "Fecha de salida")]
         public System.DateTime FechaSalida { get; set; }
+        [Display(Name = "Kilometraje de salida")]
+        [Required(ErrorMessage = "El kilometraje de salida es Requerido")]
         public int KilometrajeSalida { get; set; }
+        [Display(Name = "Fecha de ingreso")]
         public System.DateTime FechaIngresa { get; set; }
+        [Display(Name = "Kilometraje de ingreso")]
         public int KilometrajeIngresa { get; set; }
+        [Display(Name = "Anomalias")]
         public string Anomalias { get; set; }
+        public string ValorNuevo()
+        {
+            return "IdControlVehiculo:" + IdControlVehiculo + " |IdFlotilla:" + IdFlotilla + " |IdPersonal:" + IdPersonal + "| EstadoVehiculo:" + EstadoVehiculo + "| FechaSalida:" + FechaSalida + "| KilometrajeSalida:" + KilometrajeSalida + "| FechaIngresa:" + FechaIngresa + "| KilometrajeIngresa:" + KilometrajeIngresa + "| Anomalias:" + Anomalias;
+        }
+        public string ValorAntiguo(ControlVehiculo Entidad)
+        {
+            return "IdControlVehiculo:" + Entidad.IdControlVehiculo + " |IdFlotilla:" + Entidad.IdFlotilla + " |IdPersonal:" + Entidad.IdPersonal + "| EstadoVehiculo:" + Entidad.EstadoVehiculo + "| FechaSalida:" + Entidad.FechaSalida + "| KilometrajeSalida:" + Entidad.KilometrajeSalida + "| FechaIngresa:" + Entidad.FechaIngresa + "| KilometrajeIngresa:" + Entidad.KilometrajeIngresa + "| Anomalias:" + Entidad.Anomalias;
+        }
     }
 }
