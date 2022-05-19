44173b6113ccd88084382409ef0768518a9d6228
Genius501 <jarguedas@datasoftla.com>
2022-05-19 13:24:29 -0600

    s

---
+++ Sistema Control de Activos/SCA/Controllers/BitacoraMovimientosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCA.Controllers
+{
+    public class BitacoraMovimientosController : Controller
+    {
+        // GET: BitacoraMovimientos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sistema Control de Activos/SCA/Controllers/BitacoraSesionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCA.Controllers
+{
+    public class BitacoraSesionController : Controller
+    {
+        // GET: BitacoraSesion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
