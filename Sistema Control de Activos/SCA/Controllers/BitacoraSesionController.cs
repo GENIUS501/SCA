3d48e98b96888e5d95eaf9e6a9bde8e1ac107332
Genius501 <jarguedas@datasoftla.com>
2022-05-21 12:00:43 -0600

    Cambios

--- Sistema Control de Activos/SCA/Controllers/BitacoraSesionController.cs
+++ Sistema Control de Activos/SCA/Controllers/BitacoraSesionController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Filters;
+using SCA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,21 @@ namespace SCA.Controllers
 {
     public class BitacoraSesionController : Controller
     {
+        private BaseDatosSCAEntities db = new BaseDatosSCAEntities();
+        [AuthorizeUser(idmodulo: "BitacoraEntrada/Salida")]
         // GET: BitacoraSesion
-        public ActionResult Index()
+        public ActionResult Index(string Nombre, string FechaIni, string FechaFin)
         {
-            return View();
+            var Modelo = db.BitacoraIngresoSalida.ToList();
+            if (Nombre != null)
+            {
+                Modelo = Modelo.Where(x => x.Usuario.Personal.Nombre.Contains(Nombre)).ToList();
+            }
+            if (FechaIni != null && FechaFin != null)
+            {
+                Modelo = Modelo.Where(x => x.FechaIngreso >= Convert.ToDateTime(FechaIni) && x.FechaIngreso <= Convert.ToDateTime(FechaFin)).ToList();
+            }
+            return View(Modelo);
         }
     }
 }
