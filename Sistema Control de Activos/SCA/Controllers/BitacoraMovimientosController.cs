f453ee34fc62b0eac64461137504969c0c85bd9c
Genius501 <jarguedas@datasoftla.com>
2022-05-21 12:39:46 -0600

    Cambios

--- Sistema Control de Activos/SCA/Controllers/BitacoraMovimientosController.cs
+++ Sistema Control de Activos/SCA/Controllers/BitacoraMovimientosController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Filters;
+using SCA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,25 @@ namespace SCA.Controllers
 {
     public class BitacoraMovimientosController : Controller
     {
+        private BaseDatosSCAEntities db = new BaseDatosSCAEntities();
+        [AuthorizeUser(idmodulo: "BitacoraMovimientos")]
         // GET: BitacoraMovimientos
-        public ActionResult Index()
+        public ActionResult Index(string Nombre,string Tipo, string FechaIni, string FechaFin)
         {
-            return View();
+            var Modelo = db.BitacoraMovimiento.ToList();
+            if (Nombre != null)
+            {
+                Modelo = Modelo.Where(x => x.Usuario.Usuario1.Contains(Nombre)).ToList();
+            }
+            if (Tipo != null)
+            {
+                Modelo = Modelo.Where(x => x.TipoMovimiento.Contains(Tipo)).ToList();
+            }
+            if (FechaIni != null && FechaFin != null)
+            {
+                Modelo = Modelo.Where(x => x.FechaMovimiento >= Convert.ToDateTime(FechaIni) && x.FechaMovimiento <= Convert.ToDateTime(FechaFin)).ToList();
+            }
+            return View(Modelo);
         }
     }
 }
